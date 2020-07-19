f56cd388fcb0edc263183b14d241f1195fbbbf45
meaqua <1019635827@qq.com>
2020-07-19 22:47:34 +0800

    定义接口

--- common/src/main/java/util/RestAPIResult.java
+++ common/src/main/java/util/RestAPIResult.java
@@ -1,4 +1,36 @@
 package util;
+/**
+ * 统一的返回结构
+ */
+public class  RestAPIResult<T> {
+    // 返回码，0表示成功，其他表示失败,如果失败时不要填充data
+    private int ret;
+    // 返回信息
+    private String message;
+    // 返回数据
+    private T data;
 
-public class RestAPIResult {
+    public int getRet() {
+        return ret;
+    }
+
+    public void setRet(int ret) {
+        this.ret = ret;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
