d34c28138752c683b71d782a2d48381c055ad729
meaqua <1019635827@qq.com>
2020-07-26 15:27:50 +0800

    添加 公共类 IsAliveResponseData

---
+++ common/src/main/java/response/IsAliveResponseData.java
@@ -0,0 +1,31 @@
+package response;
+
+public class IsAliveResponseData {
+    private int userID;
+    private String name;
+    private boolean isAlive;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+}
